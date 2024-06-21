0b5ec2efcfeef6e60e3e12038e67b732185aef21
Sajjad Hossain Sunny <122621992+sajjadsunny-dev@users.noreply.github.com>
2024-06-21 12:10:25 +0600

    service card fix

--- src/app/core/ServicesCard/ServicesCard.tsx
+++ src/app/core/ServicesCard/ServicesCard.tsx
@@ -11,7 +11,7 @@ const ServicesCard: FC<Props> = ({ serviceIcon, serviceName, serviceDetailes }) 
   return (
     <div className="col-span-2 landscape:col-span-1 md:col-span-1 group rounded-xl shadow-lg py-5 pl-4 pr-5 md:pr-4 overflow-hidden cursor-pointer relative before:absolute before:top-1/2 before:right-0 before:-translate-y-1/2 before:bg-primary before:w-2.5 lg:before:w-3 before:h-32 lg:before:h-20 before:rounded-s-md lg:before:rounded-s-lg before:duration-500 hover:before:w-full hover:before:h-full hover:shadow-xl">
       <div className="relative z-20 flex items-center lg:items-start flex-col lg:flex-row gap-4">
-        <div className="w-16 h-16 rounded-md overflow-hidden shadow-serviceShadow">
+        <div className="w-16 h-16 landscape:lg:w-12 landscape:lg:h-12 landscape:xl:w-16 landscape:xl:h-16 rounded-md overflow-hidden shadow-serviceShadow">
           <Image src={serviceIcon} alt="serviceIcon" width={100} height={100} />
         </div>
         <div className="landscape:lg:w-[300px] landscape:xl:w-[385px] lg:w-[385px]">
