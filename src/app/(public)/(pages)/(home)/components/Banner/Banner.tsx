b98318abb639a0d9967d67a14ce748087f7a6447
Sajjad Hossain Sunny <122621992+sajjadsunny-dev@users.noreply.github.com>
2024-07-12 22:13:53 +0600

    banner font fix

--- src/app/(public)/(pages)/(home)/components/Banner/Banner.tsx
+++ src/app/(public)/(pages)/(home)/components/Banner/Banner.tsx
@@ -10,7 +10,7 @@ const Banner: FC = () => {
       <div className="bg-hero-pattern h-dvh bg-cover bg-no-repeat bg-center relative before:content-[''] before:absolute before:top-0 before:left-0 before:bg-overlay before:w-full before:h-full grid place-content-center">
         <div className="relative z-10 text-white capitalize text-center px-2.5">
           <h2 className="text-xl md:text-28 leading-50 font-medium inline-block relative after:absolute after:bottom-0 after:right-0 after:translate-x-4 md:after:translate-x-6 after:-translate-y-[18px] md:after:-translate-y-4 after:w-3.5 md:after:w-5 after:h-0.5 after:bg-white mb-2 md:mb-14">hello</h2>
-          <h1 className="text-4xl md:text-6xl leading-[55px] md:leading-[83px] font-semibold mb-4 text-center normal-case">
+          <h1 className="text-5xl md:text-6xl leading-[55px] md:leading-[83px] font-semibold mb-4 text-center normal-case">
             I&apos;m <span className='text-primary'>
               <Typewriter
                 words={Banner_Contents.heading}
--- src/statics/Banner_Contents.tsx
+++ src/statics/Banner_Contents.tsx
@@ -1,4 +1,4 @@
 export const Banner_Contents = {
-  heading: ["Sajjad Hossain Sunny.", "a Web Designer.", "a Front-end Developer.", "a React Developer."],
+  heading: ["Sajjad Hossain Sunny.", "a Web Designer.", "a FrontEnd Developer.", "a React Developer."],
   location: 'dhaka, bangladesh.',
 }
