be645bde8e672befeb319165a12b29ea3f192e4b
Sajjad Hossain Sunny <122621992+sajjadsunny-dev@users.noreply.github.com>
2024-11-28 10:18:50 +0600

    spinner

--- src/components/ui/splash-screen/SplashScreen.tsx
+++ src/components/ui/splash-screen/SplashScreen.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Icons } from '../icons'
 
 const SplashScreen = () => {
   return (
       <div className="fixed top-0 left-0 w-full h-dvh flex items-center justify-center">
-        <span className='text-3xl text-secondary-600'>Loading...</span>
+        <span className='text-3xl text-secondary-600 spin-icon'><Icons.FavIcon className='w-16 sm:w-20 lg:w-28' /></span>
       </div>
   )
 }
